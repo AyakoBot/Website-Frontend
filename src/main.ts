f307ff1e0761748b426fb740802535640bb5c91c
Lars_und_so <46791248+Larsundso@users.noreply.github.com>
2023-05-16 21:54:35 +0200

    lazy loading

--- src/main.ts
+++ src/main.ts
@@ -5,28 +5,19 @@ import './style.css';
 import App from './App.vue';
 
 import home from './pages/home.vue';
-import premium from './pages/premium.vue';
-import credits from './pages/credits.vue';
-import privacy from './pages/privacy.vue';
-import terms from './pages/terms.vue';
-import invite from './pages/invite.vue';
-import support from './pages/support.vue';
-import appeals from './pages/appeals.vue';
-import login from './pages/login.vue';
-import unknownPage from './pages/404.vue';
 
 const router = VueRouter.createRouter({
  history: VueRouter.createWebHistory(),
  routes: [
   { path: '/', component: home },
-  { path: '/premium', component: premium },
-  { path: '/credits', component: credits },
-  { path: '/invite', component: invite },
-  { path: '/support', component: support },
-  { path: '/privacy', component: privacy },
-  { path: '/terms', component: terms },
-  { path: '/appeals', component: appeals },
-  { path: '/login', component: login },
+  { path: '/premium', component: () => import('./pages/premium.vue') },
+  { path: '/credits', component: () => import('./pages/credits.vue') },
+  { path: '/invite', component: () => import('./pages/invite.vue') },
+  { path: '/support', component: () => import('./pages/support.vue') },
+  { path: '/privacy', component: () => import('./pages/privacy.vue') },
+  { path: '/terms', component: () => import('./pages/terms.vue') },
+  { path: '/appeals', component: () => import('./pages/appeals.vue') },
+  { path: '/login', component: () => import('./pages/login.vue') },
   {
    path: '/servers/:guildid/appeal',
    component: () => import('./pages/servers/appeal.vue'),
@@ -39,7 +30,7 @@ const router = VueRouter.createRouter({
    path: '/servers/:guildid/appeal/:punishmentid/success',
    component: () => import('./pages/servers/appealSuccess.vue'),
   },
-  { path: '/:pathMatch(.*)*', component: unknownPage },
+  { path: '/:pathMatch(.*)*', component: () => import('./pages/404.vue') },
  ],
 });
 
